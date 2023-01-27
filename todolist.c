af7f3e74fcbe73cb4b19b35cab60f4a51dad799f
jakos <jakosvadim@gmail.com>
2023-01-27 12:32:27 +0300

    Add get_command and parse_command

--- todolist.c
+++ todolist.c
@@ -2,6 +2,8 @@
 #include "todolist_view.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void todolist_init(struct todolist *list)
 {
@@ -25,17 +27,67 @@ static void show_list(const struct todolist *list)
         show_task_list(&list->tasks);
 }
 
+static void get_command(char *cmd, int len)
+{
+    int c, i;
+    i = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n' || i >= len - 1)
+            break;
+        cmd[i] = c; 
+        i++;
+    }
+    cmd[i] = '\0';
+}
+
+static void parse_command(const char *cmd, char **params, int size,
+                                                        int *params_cnt)
+{
+    int i, param_len, cmd_len;
+    char *param;
+    cmd_len = strlen(cmd);
+    param = malloc(cmd_len + 1);
+    param_len = 0;
+    *params_cnt = 0;
+    for (i = 0; cmd[i]; i++) {
+        if (*params_cnt >= size)
+            break;
+        if (cmd[i] == ' ' && param_len > 0) {
+            param[param_len] = '\0';
+            strlcpy(params[*params_cnt], param, param_len + 1);
+            (*params_cnt)++;
+            param_len = 0;
+        } else if (cmd[i] != ' ') {
+            param[param_len] = cmd[i];
+            param_len++;
+        }
+    }
+    if (param_len > 0) {
+        param[param_len] = '\0';
+        strlcpy(params[*params_cnt], param, param_len + 1);
+        (*params_cnt)++;
+    }
+    free(param);
+}
+
 void todolist_main_loop(struct todolist *list)
 {
-    char cmd;
+    enum { max_cmd_len = 200, max_params_cnt = 10 };
+    char cmd[max_cmd_len];
+    char *params[max_params_cnt];
+    int params_cnt, i;
+    for (i = 0; i < max_params_cnt; i++)
+        params[i] = malloc(max_cmd_len);
     do {
         show_list(list);
+        
+        get_command(cmd, max_cmd_len);
+        parse_command(cmd, params, max_params_cnt, &params_cnt); 
+        printf("Params count: %d\n", params_cnt);
+        for (i = 0; i < params_cnt; i++)
+            printf("Param [%d]: '%s'\n", i, params[i]);
 
-        do {
-            cmd = getchar();
-        } while (cmd == ' ' || cmd == '\n');
-
-        switch (cmd) {
+        switch (params[0][0]) {
         case 'a':
             break;
         case 't':
@@ -51,5 +103,8 @@ void todolist_main_loop(struct todolist *list)
         default:
             break;
         }
-    } while (cmd != 'q');
+    } while (params[0][0] != 'q');
+
+    for (i = 0; i < max_params_cnt; i++)
+        free(params[i]);
 }
